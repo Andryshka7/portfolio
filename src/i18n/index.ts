df51abaac2b29c340cd7b5f27fc8daf241ea2603
Andryshka7 <a.lapchik7@gmail.com>
2024-06-04 19:39:14 +0300

    fixed i18n type errors

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -10,7 +10,7 @@ type NestedObject = {
     [key: string]: string | NestedObject
 }
 
-const returnNested = (object: NestedObject, nestedKey: string) => {
+const returnNested = (object: NestedObject, nestedKey: string) : string => {
     if (!nestedKey.includes('.')) return object[nestedKey] as string
 
     const key = nestedKey.slice(0, nestedKey.indexOf('.'))
