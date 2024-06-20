4254452a02026e8046df4f98c479593cae38d623
Andryshka16 <a.lapchik7@gmail.com>
2024-06-21 02:50:01 +0300

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+import netlify from "@astrojs/netlify";
 
-import tailwind from '@astrojs/tailwind'
-import icon from 'astro-icon'
-
+// https://astro.build/config
 export default defineConfig({
-    prefetch: true,
-    site: import.meta.env.PUBLIC_URL,
-    integrations: [tailwind(), icon({ iconDir: 'public/icons' })],
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'ru', 'lv']
-    }
-})
+  prefetch: true,
+  site: import.meta.env.PUBLIC_URL,
+  integrations: [tailwind(), icon({
+    iconDir: 'public/icons'
+  })],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ru', 'lv']
+  },
+  output: "server",
+  adapter: netlify()
+});
