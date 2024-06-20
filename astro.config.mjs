722b14d8a45a7688b8bc1b972c3ed055ce95adc1
Andryshka16 <a.lapchik7@gmail.com>
2024-06-20 15:11:15 +0300

    refactored & fixed icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
+
 import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 
 export default defineConfig({
     prefetch: true,
-    integrations: [tailwind()],
+    integrations: [tailwind(), icon({ iconDir: 'public/icons' })],
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'ru', 'lv']
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,7 +2,6 @@ export { default as MobileNavbar } from './Navbar/MobileNavbar.astro'
 export { default as DesktopNavbar } from './Navbar/DesktopNavbar.astro'
 
 export { default as Alert } from './Alert.astro'
-export { default as Icon } from './Icon.astro'
 export { default as ImageCarousel } from './ImageCarousel.astro'
 
 export { default as Footer } from './Footer.astro'
